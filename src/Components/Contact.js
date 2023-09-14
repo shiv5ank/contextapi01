471a059cf7c2c5a6dec087412e8f21186695bf73
shiv5ank <Shivank0596@gmail.com>
2023-09-14 20:47:14 +0530

    context

---
+++ src/Components/Contact.js
@@ -0,0 +1,17 @@
+import React, { useContext } from "react";
+import Navbar from "./Navbar";
+import ThemeContext from "../Context";
+import "../App.css";
+const Contact = () => {
+  const { isLight } = useContext(ThemeContext);
+  return (
+    <>
+      <div className={isLight ? "light" : "dark"}>
+        <Navbar />
+
+        <h1>Contact me at shivank0596@gmail.com</h1>
+      </div>
+    </>
+  );
+};
+export default Contact;
